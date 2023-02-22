605f14dcbb98026c8d38e042108177e89898f594
desarrollo <desarrollo@academicos.cesde.edu.co>
2023-02-22 08:47:08 -0500

    segunda parte

--- src/controllers/controladorhome.js
+++ src/controllers/controladorhome.js
@@ -1,7 +1,29 @@
-//definiendo datos
-let nombre = "tiburonsin"
-if(5>2)
-{
-    let nombre = "lucho dias"
-}
-console.log("hola "+nombre)
+import {productos} from "../helpers/baseDatos.js"
+
+//llamando a la fila
+let fila = document.getElementById("fila")
+
+//recorrer arreglo
+productos.forEach(function(producto){
+    
+
+    //creando columnas para alojar los productos
+    let columna = document.createElement("div")
+    columna.classList.add("col")
+
+    //crear tarjeta para almacenar la info del producto
+    let tarjeta = document.createElement("div")
+    //card es una clase de boostrap, calse de bootstrap (heigth)h-100
+    tarjeta.classList.add("card", "h-100")
+
+    //crear imagen
+    let imagen = document.createElement("img")
+    //imgfluid es una clase de boostrap que hace la imagen responsive
+    imagen.classList.add("img-fluid", "w-100")
+    imagen.src = producto.fotos[0]
+
+    //padres e hijos
+    tarjeta.appendChild(imagen)
+    columna.appendChild(tarjeta)
+    fila.appendChild(columna)
+})
---
+++ src/helpers/baseDatos.js
@@ -0,0 +1,40 @@
+//set una base de datos
+
+//el nombre de un arreglo va en plural
+export let productos = Array(
+    {
+        nombre:"producto uno",
+        descripcion:"pon la descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto1.avif?alt=media&token=4ac79242-a51c-4502-8205-2b48fb4da213","na2"],
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"producto uno",
+        descripcion:"pon la descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto2.webp?alt=media&token=7360cdf4-91b5-4245-b5b8-6123a5c113a2","na2"],
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"producto uno",
+        descripcion:"pon la descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto3.jpg?alt=media&token=cc4b810c-05f8-4bcf-95e0-4ddc6b6b10b2","na2"],
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"producto uno",
+        descripcion:"pon la descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto3.jpg?alt=media&token=cc4b810c-05f8-4bcf-95e0-4ddc6b6b10b2","na2"],
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"producto uno",
+        descripcion:"pon la descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto5.jpg?alt=media&token=c9bab249-64b6-4e17-8f96-bf5da106efef","na2"],
+        precio:70000,
+        popularidad:4.2
+    }
+)
