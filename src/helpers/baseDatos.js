0447f8bbe910eeb3e69e0b2fe6fe2301b31bac77
santiago <santiagomunoz735@gmail.com>
2023-02-27 18:55:01 -0500

    imgs subidas

--- src/helpers/baseDatos.js
+++ src/helpers/baseDatos.js
@@ -5,36 +5,72 @@ export let productos = Array(
     {
         nombre:"producto uno",
         descripcion:"pon la descripcion",
-        fotos:["https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto1.avif?alt=media&token=4ac79242-a51c-4502-8205-2b48fb4da213","na2"],
+        fotos:["https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto1.avif?alt=media&token=4ac79242-a51c-4502-8205-2b48fb4da213","https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto1.2.jpg?alt=media&token=34ca83ca-0503-4342-abc6-72657c424a7f"],
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"producto dos",
+        descripcion:"pon la descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto2.webp?alt=media&token=7360cdf4-91b5-4245-b5b8-6123a5c113a2","https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto2.2.png?alt=media&token=faa3bcf7-31f3-40b8-936a-5b78066c0ecf"],
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"producto tres",
+        descripcion:"pon la descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto3.jpg?alt=media&token=cc4b810c-05f8-4bcf-95e0-4ddc6b6b10b2","https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto3.2.jpg?alt=media&token=ec2d6326-8f23-418e-a8fb-a2c4987967bc"],
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"producto cuatro",
+        descripcion:"pon la descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto4.webp?alt=media&token=665766f1-fc08-4cd4-9272-c6802de62990","https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto4.2.png?alt=media&token=74a5ba72-7e25-4615-b1a2-2a13552fd6e8"],
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"producto cinco",
+        descripcion:"pon la descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto5.jpg?alt=media&token=10fff5b2-9c79-4563-b635-7fa2be6cd950","https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto5.2.jpg?alt=media&token=1fd0e9e9-c5a4-44b4-8a4a-8f1da863d874"],
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"producto seis",
+        descripcion:"pon la descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto6.png?alt=media&token=d14f7672-efec-473e-be83-585687104d6b","https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto6.2.png?alt=media&token=42069a95-46d4-4ac2-8018-6b9cdce4464c"],
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"producto siete",
+        descripcion:"pon la descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto7.webp?alt=media&token=ad23ca1d-bd0a-413e-a97f-4b6a69b9f8ee","https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto7.2.png?alt=media&token=d9d22967-7c01-4c19-b118-1a69f7af6132"],
+        precio:70000,
+        popularidad:4.2
+    },
+    {
+        nombre:"producto ocho",
+        descripcion:"pon la descripcion",
+        fotos:["https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto8.jpg?alt=media&token=dbee24ae-c827-4c6f-ad43-60b66687fd40","https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto8.2.jpg?alt=media&token=6ff37d25-401f-4858-b7dd-e426ac44723a"],
         precio:70000,
         popularidad:4.2
     },
     {
         nombre:"producto uno",
         descripcion:"pon la descripcion",
-        fotos:["https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto2.webp?alt=media&token=7360cdf4-91b5-4245-b5b8-6123a5c113a2","na2"],
+        fotos:["https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto9.jpg?alt=media&token=aabf9d2e-7723-4686-85f9-b17eba1bc429","https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto9.2.jpg?alt=media&token=f0940dd3-c08d-44cf-8a29-7246baea6023"],
         precio:70000,
         popularidad:4.2
     },
     {
         nombre:"producto uno",
         descripcion:"pon la descripcion",
-        fotos:["https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto3.jpg?alt=media&token=cc4b810c-05f8-4bcf-95e0-4ddc6b6b10b2","na2"],
-        precio:70000,
-        popularidad:4.2
-    },
-    {
-        nombre:"producto uno",
-        descripcion:"pon la descripcion",
-        fotos:["https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto3.jpg?alt=media&token=cc4b810c-05f8-4bcf-95e0-4ddc6b6b10b2","na2"],
-        precio:70000,
-        popularidad:4.2
-    },
-    {
-        nombre:"producto uno",
-        descripcion:"pon la descripcion",
-        fotos:["https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto5.jpg?alt=media&token=c9bab249-64b6-4e17-8f96-bf5da106efef","na2"],
+        fotos:["https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/produto10.webp?alt=media&token=3b1a2763-86af-4c02-bf5b-349871fad8ae","https://firebasestorage.googleapis.com/v0/b/smmtienda-9dce6.appspot.com/o/producto10.2.png?alt=media&token=5debf2c9-19d6-46bb-82d0-8ff401aaa80e"],
         precio:70000,
         popularidad:4.2
     }
+    
 )
